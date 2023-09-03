2afda0dd3e99491d8ec8245454586f857e2b74cd
Robert P <porobertdev@gmail.com>
2023-09-03 18:11:52 +0300

    Create click events for the buttons
    * Add 'num' class for the numbered buttons, so we can check the type
    of the button pressed, and do actions based on that.

--- scripts.js
+++ scripts.js
@@ -13,3 +13,30 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function createClickEvents() {
+    const buttons = document.querySelectorAll('.btn');
+    buttons.forEach( btn => btn.addEventListener('click', clickHandler));
+
+    function clickHandler(event) {
+        let value = event.target.textContent;
+        let btnType = event.target.classList[1];
+        console.log(value);
+
+        if (operation.length == 0 || operators.includes(operation.at(-1)) || btnType == 'operator') {
+            operation.push(value)
+        } else if (btnType == 'num') {
+            operation[operation.length - 1] += value;
+        }
+
+        if (operation.length == 4) {
+             operation.splice(0, 3, operate(+operation[0], operation[1], +operation[2]));
+        }
+
+        console.log(operation);
+    }
+}
+
+let operators = ['+', '-', 'x', '÷'];
+let operation = [];
+createClickEvents();
